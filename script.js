40bb254893c383dd818f1d6cab978246e949d6e0
Neftalem <dneftalem@gmail.com>
2025-05-09 16:39:36 +0300

    add js code

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const textbox = document.getElementById("textbox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const resalt = document.getElementById("resalt");
+
