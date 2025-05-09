dfe673a3e1c81b5ae7349ad06fcd8a1d1f950312
Neftalem <dneftalem@gmail.com>
2025-05-09 16:51:55 +0300

    add on a button onCklick add if and else

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const toFahrenheit = document.getElementById("toFahrenheit");
 const toCelsius = document.getElementById("toCelsius");
 const resalt = document.getElementById("resalt");
 
+function convert() {
+
+    if(toFahrenheit.checked){
+
+        resalt.textContent = "You are selected to Fahrenheit"
+
+    }
+
+    else if(toCelsius.checked) {
+        resalt.textContent = "You are selected to Celsius"
+    }
+
+    else {
+        resalt.textContent = "Select a unit"
+    }
+
+}
